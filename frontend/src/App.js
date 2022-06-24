c3b6384f4c419639ce8225091db6143563ba94e9
Dan <dan_b_luu@yahoo.com>
2022-06-23 20:39:37 -0500

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+
+export default function App() {
+    return (
+        <div className="App">
+            <header>
+                <a href="/">amazona</a>
+            </header>
+
+            <main>
+                list products
+            </main>
+        </div>
+    )
+}
